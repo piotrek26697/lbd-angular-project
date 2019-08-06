bed0ab801927d6adb238a72a3c45efd0954a2da3
piotr.musiol <piotrek26697@gmail.com>
2019-08-06 16:08:45 +0200

    Basic Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnimalComponent } from './animal-tracking/animal/animal.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/animals', pathMatch: 'full' },
+  { path: 'animals', component: AnimalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppServicesModule } from './app-services/app-services.module';
+import { AnimalTrackingModule } from './animal-tracking/animal-tracking.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppServicesModule,
+    AnimalTrackingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
