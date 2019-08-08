c1cb74bc08ae5b45c750de5943be967da4729cf4
piotr.musiol <piotrek26697@gmail.com>
2019-08-08 13:24:28 +0200

    Implement http errors handling

--- src/app/app-services/animal.service.ts
+++ src/app/app-services/animal.service.ts
@@ -15,24 +15,37 @@ export class AnimalService {
 
   constructor(private httpClient: HttpClient) { }
 
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error): Observable<T> => {
+
+      console.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
   getAnimals(): Observable<Animal[]> {
     return this.httpClient.get<AnimalAttributes[]>(this.apiURL)
-      .pipe(map(a => a.map(a => new Animal(a))));
+      .pipe(map(a => a.map(a => new Animal(a))),
+        catchError(this.handleError<Animal[]>("getAnimals", new Array())));
   }
 
   getAnimal(id: number): Observable<Animal> {
     return this.httpClient.get<AnimalAttributes>(this.apiURL + `/${id}`)
-      .pipe(map(a => new Animal(a)))
+      .pipe(map(a => new Animal(a)),
+        catchError(this.handleError<Animal>("getAnimal", null)));
   }
 
   updateAnimal(animal: Animal): Observable<any> {
     return this.httpClient.put(this.apiURL + `/${animal.id}`, animal, this.httpOptions)
+      .pipe(catchError(this.handleError<any>("updateAnimal", new Object())));
   }
 
   deleteAnimal(animal: Animal): Observable<any> {
-    return this.httpClient.delete(this.apiURL + `/${animal.id}`, this.httpOptions);
+    return this.httpClient.delete(this.apiURL + `/${animal.id}`, this.httpOptions)
+      .pipe(catchError(this.handleError<any>("deleteAnimal", new Object())));
   }
-  postAnimal(animal: Animal): Observable<any>{
-    return this.httpClient.post(this.apiURL, animal, this.httpOptions);
+  postAnimal(animal: Animal): Observable<any> {
+    return this.httpClient.post(this.apiURL, animal, this.httpOptions)
+      .pipe(catchError(this.handleError<any>("postAnimal", new Object())));
   }
 }
